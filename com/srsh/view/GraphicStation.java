103a86f78440451569603e46af355686ab08e282
erosa <erosakos02@gmail.ocm>
2017-05-05 16:01:34 +0200

    Addcomponent done

--- com/srsh/view/GraphicStation.java
+++ com/srsh/view/GraphicStation.java
@@ -2,12 +2,24 @@ package com.srsh.view;
 
 import com.srsh.model.Station;
 
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by erosa on 2017. 05. 05..
  */
 public class GraphicStation extends Drawable {
 
     protected Station station;
+
+    static{
+        try {
+            img = ImageIO.read(new File("resources/station.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     GraphicStation(Station station){
         this.station = station;
