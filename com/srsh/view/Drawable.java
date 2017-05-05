12f6d6f4ed5c3bae4c457aceefbf549e7ea1a78d
erosa <erosakos02@gmail.ocm>
2017-05-05 15:00:15 +0200

    Graphical components skeleton.

--- com/srsh/view/Drawable.java
+++ com/srsh/view/Drawable.java
@@ -1,7 +1,10 @@
 package com.srsh.view;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by erosa on 2017. 05. 05..
@@ -31,6 +34,15 @@ public class Drawable {
     public void draw(JPanel panel){
         JLabel picLabel = new JLabel(new ImageIcon(this.img));
         panel.add(picLabel);
+    }
+
+    /**
+     * Set img
+     * @param src
+     * @throws IOException
+     */
+    protected void setImg(String src) throws IOException {
+        this.img = ImageIO.read(new File(src));
     }
 
 }
--- com/srsh/view/GraphicCoalWagon.java
+++ com/srsh/view/GraphicCoalWagon.java
@@ -2,11 +2,12 @@ package com.srsh.view;
 
 import com.srsh.model.CoalWagon;
 
+import java.io.IOException;
+
 /**
  * Created by erosa on 2017. 05. 05..
  */
 public class GraphicCoalWagon extends Drawable {
 
     protected CoalWagon coalwagon;
-
 }
