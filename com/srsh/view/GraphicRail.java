795663b9b9ca38dbbb029ce4e04aa5b412dbe209
erosa <erosakos02@gmail.ocm>
2017-05-05 15:50:13 +0200

    Addcomponent done

--- com/srsh/view/GraphicRail.java
+++ com/srsh/view/GraphicRail.java
@@ -2,6 +2,11 @@ package com.srsh.view;
 
 import com.srsh.model.Rail;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by erosa on 2017. 05. 05..
  */
@@ -9,8 +14,9 @@ public class GraphicRail extends Drawable {
 
     protected Rail rail;
 
-    GraphicRail(Rail rail){
+    GraphicRail(Rail rail, String img) throws IOException {
         this.rail = rail;
+        this.img = ImageIO.read(new File(img));
     }
 
 }
